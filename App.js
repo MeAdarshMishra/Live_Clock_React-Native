ea40e89c99286964451be12fddcbb8f847e51544
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-07-11 00:35:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import LiveClock from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+      Live Clock
+      </Text>
+      <Card>
+        <LiveClock/>
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
